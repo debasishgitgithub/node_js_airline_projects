99570383f5ad275dc18bf74df32dec2ee1836d07
debasish <ddas1112000@gmail.com>
2026-04-15 22:03:32 +0530

    set base url in server config file as per environment

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
 module.exports = {
-    PORT : process.env.PORT,
-    BASE_URL : `http://localhost:${process.env.PORT}`,
+    PORT: process.env.PORT,
+    BASE_URL: process.env.NODE_ENV !== 'production' ? process.env.DEV_BASE_URL : process.env.PROD_BASE_URL,
 }
